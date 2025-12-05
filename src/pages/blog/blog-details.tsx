e9d7dd61e63525177ec91ef854d9a2e4dd604f8b
Talha <haidarahmd@gmail.com>
2025-12-05 23:00:12 +0500

    Pass currentSlug to related posts

--- src/pages/blog/blog-details.tsx
+++ src/pages/blog/blog-details.tsx
@@ -6,14 +6,12 @@ import { ScrollSmoother, ScrollTrigger, SplitText } from "@/plugins";
 import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
-// internal imports
 import Wrapper from "@/layouts/wrapper";
 import HeaderEleven from "@/layouts/headers/header-eleven";
 import FooterTwo from "@/layouts/footers/footer-two";
 import BlogDetailsArea from "@/components/blog/details/blog-details-area";
 import BlogDetailsBreadcrumb from "@/components/blog/details/blog-details-breadcrumb";
 import BlogDetailsRelatedPosts from "@/components/blog/details/blog-details-related-posts";
-// animation
 import { charAnimation } from "@/utils/title-animation";
 import { IdProps } from "@/types/custom-d-t";
 
@@ -27,31 +25,23 @@ const BlogDetailsMain = ({ id }: IdProps) => {
     return () => clearTimeout(timer);
   }, []);
 
+  const currentSlug = String(id); // works whether id is number or string
+
   return (
     <Wrapper>
-      {/* header area start */}
       <HeaderEleven transparent={true} />
-      {/* header area end */}
 
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <main>
-            {/* blog details hero */}
             <BlogDetailsBreadcrumb id={id} />
-            {/* blog details hero */}
-
-            {/* blog details area – now receives id/slug */}
             <BlogDetailsArea id={id} />
-            {/* blog details area */}
 
-            {/* related posts */}
-            <BlogDetailsRelatedPosts />
-            {/* related posts */}
+            {/* related posts now receives required prop */}
+            <BlogDetailsRelatedPosts currentSlug={currentSlug} />
           </main>
 
-          {/* footer area */}
           <FooterTwo topCls="" />
-          {/* footer area */}
         </div>
       </div>
     </Wrapper>
