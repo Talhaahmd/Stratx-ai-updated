299ce3d4b1f403052bd65961fa025419c25629a2
Talha <haidarahmd@gmail.com>
2025-12-05 22:34:13 +0500

    Fix final changes

--- src/app/(portfolio-details)/portfolio-showcase-details/page.tsx
+++ src/app/(portfolio-details)/portfolio-showcase-details/page.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Metadata } from "next";
 import PortfolioDetailsShowcaseMain from "@/pages/portfolio/details/portfolio-showcase-details-main";
 
-
 export const metadata: Metadata = {
   title: "Liko - Portfolio Details Showcase page",
 };
 
-const PortfolioDetailsShowcasePage = () => {
-  return (
-    <PortfolioDetailsShowcaseMain/>
-  );
-};
-
-export default PortfolioDetailsShowcasePage;
+export default function PortfolioDetailsShowcasePage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  return <PortfolioDetailsShowcaseMain params={params} />;
+}
