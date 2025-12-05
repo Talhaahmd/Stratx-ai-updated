5f9904a6ccb1a1babf8bb5ac700e3533a6c94198
Talha <haidarahmd@gmail.com>
2025-12-05 22:32:28 +0500

    Fix portfolio details page params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import HomeOnePage from "./(homes)/home-1/page";
 
 export const metadata: Metadata = {
-  title: "Liko - Home Page",
+  title: "StratX AI",
 };
 
 export default function Home() {
