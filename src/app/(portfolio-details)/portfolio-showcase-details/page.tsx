9964bb63a33f96f61249cbf34d5031babd296451
Talha Ahmd <talhaahmd@Talhas-MacBook-Air.local>
2025-12-29 03:57:47 +0500

    Fix: specific prerendering error by moving component out of pages directory

--- src/app/(portfolio-details)/portfolio-showcase-details/page.tsx
+++ src/app/(portfolio-details)/portfolio-showcase-details/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Metadata } from "next";
-import PortfolioDetailsShowcaseMain from "@/pages/portfolio/details/portfolio-showcase-details-main";
+import PortfolioDetailsShowcaseMain from "@/components/portfolio/details/portfolio-showcase-details-main";
 
 export const metadata: Metadata = {
   title: "Liko - Portfolio Details Showcase page",
